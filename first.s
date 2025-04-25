8892d9b8e286dbfc4304ec0fae000ec795fd86c5
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-04-25 18:05:49 +0545

    updated: first

--- first.s
+++ first.s
@@ -6,7 +6,7 @@
 
 _start:
 # preparing to exit the program
-  movl $1,%eax # loading 1 to the eax register 
+  movl $1,%eax # loading 1 to the eax register, the dollar sign indicate it's in immediate  addressing mode i.e. (the specified value is directly written into the said register)
 
   movl $9,%ebx # specifying the status code the program needs to exit in (can be any number)
 
